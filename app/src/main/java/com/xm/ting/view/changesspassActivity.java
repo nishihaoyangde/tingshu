0368e2d2ae7c2c8d9737c6e34b01f82c8761c96f
Administrator <383067690@qq.com>
2017-02-28 16:29:11 +0800

    jintao

---
+++ app/src/main/java/com/xm/ting/view/changesspassActivity.java
@@ -0,0 +1,18 @@
+package com.xm.ting.view;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.xm.ting.R;
+
+/**
+ * Created by Administrator on 2017/2/27.
+ */
+
+public class changesspassActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_changepass);
+    }
+}
